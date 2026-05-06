277cc6dacb1f52a3e4bdf82e6b661ff25d072691
Fabian <112954@alumnouninter.mx>
2026-05-06 17:03:20 -0600

    Agregar archivos de proyecto.

---
+++ API2/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace API2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.IDTXT = new System.Windows.Forms.TextBox();
+            this.colorTXTS = new System.Windows.Forms.TextBox();
+            this.descripTXT = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 340);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "REGISTROS";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(164, 340);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(111, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "CREAR";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(312, 340);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(111, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "ELIMINAR";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(465, 340);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(108, 24);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "ACTULIZAR";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ID";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 88);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "COLOR";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 152);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "DESCRIPCION";
+            // 
+            // IDTXT
+            // 
+            this.IDTXT.Location = new System.Drawing.Point(50, 24);
+            this.IDTXT.Name = "IDTXT";
+            this.IDTXT.Size = new System.Drawing.Size(100, 22);
+            this.IDTXT.TabIndex = 7;
+            // 
+            // colorTXTS
+            // 
+            this.colorTXTS.Location = new System.Drawing.Point(84, 85);
+            this.colorTXTS.Name = "colorTXTS";
+            this.colorTXTS.Size = new System.Drawing.Size(100, 22);
+            this.colorTXTS.TabIndex = 8;
+            // 
+            // descripTXT
+            // 
+            this.descripTXT.Location = new System.Drawing.Point(116, 149);
+            this.descripTXT.Name = "descripTXT";
+            this.descripTXT.Size = new System.Drawing.Size(100, 22);
+            this.descripTXT.TabIndex = 9;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(312, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(476, 213);
+            this.dataGridView1.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.descripTXT);
+            this.Controls.Add(this.colorTXTS);
+            this.Controls.Add(this.IDTXT);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox IDTXT;
+        private System.Windows.Forms.TextBox colorTXTS;
+        private System.Windows.Forms.TextBox descripTXT;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ API2/Form1.cs
@@ -0,0 +1,130 @@
+﻿using API2.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace API2
+{
+    public partial class Form1 : Form
+    {
+        private readonly string apiUrl = "https://localhost:7225/api/Tabla2";
+        private readonly HttpClient _httpClient;
+
+        public Form1()
+        {
+            InitializeComponent();
+            _httpClient = new HttpClient();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            await listaRegistros();
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            await CrearRegistro();
+            await listaRegistros();
+        }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            await EliminarRegistro();
+            await listaRegistros();
+        }
+
+        private async void button4_Click(object sender, EventArgs e)
+        {
+            await ActualizarRegistro();
+            await listaRegistros();
+        }
+
+        private async Task listaRegistros()
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync(apiUrl);
+
+            if (response.IsSuccessStatusCode)
+            {
+                string data = await response.Content.ReadAsStringAsync();
+
+                try
+                {
+                    var registros = JsonConvert.DeserializeObject<List<Tabla2>>(data);
+                    dataGridView1.DataSource = registros;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    MessageBox.Show(data);
+                }
+            }
+            else
+            {
+                MessageBox.Show($"Error: {response.ReasonPhrase}");
+            }
+
+        }
+
+        private async Task CrearRegistro()
+        {
+            Tabla2 registro = new Tabla2
+            {
+                Id = int.Parse(IDTXT.Text),
+                Color = colorTXTS.Text,
+                Descripcion = descripTXT.Text
+            };
+
+            string jsonData = JsonConvert.SerializeObject(registro);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+
+            var response = await _httpClient.PostAsync(apiUrl, content);
+
+            if (response.IsSuccessStatusCode)
+                MessageBox.Show("Creado");
+            else
+                MessageBox.Show("Error");
+        }
+
+        private async Task ActualizarRegistro()
+        {
+            int id = int.Parse(IDTXT.Text);
+
+            Tabla2 registroActualizado = new Tabla2
+            {
+                Id = id,
+                Color = colorTXTS.Text,
+                Descripcion = descripTXT.Text
+            };
+
+            string jsonData = JsonConvert.SerializeObject(registroActualizado);
+            StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            var response = await _httpClient.PutAsync($"{apiUrl}/{id}", content);
+
+            if (response.IsSuccessStatusCode)
+                MessageBox.Show("Actualizado");
+            else
+                MessageBox.Show("Error");
+        }
+
+        private async Task EliminarRegistro()
+        {
+            int id = int.Parse(IDTXT.Text);
+
+            HttpResponseMessage response = await _httpClient.DeleteAsync($"{apiUrl}/{id}");
+
+            if (response.IsSuccessStatusCode)
+            {
+                MessageBox.Show("Registro eliminado exitosamente", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show($"Error al eliminar registro: {response.ReasonPhrase}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
